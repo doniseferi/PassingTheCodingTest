9c7e9c0be7e0a3f81b42b6a09c71e7109d750b93
doniseferi <doni.seferi@gmail.com>
2023-09-07 19:22:55 +0200

    weave

--- PassingTheCodingTestQuestions/BstSequenceIterative.cs
+++ PassingTheCodingTestQuestions/BstSequenceIterative.cs
@@ -1,5 +1,30 @@
 namespace PassingTheCodingTestQuestions;
 
-internal class BstSequenceIterative
+internal static class BstSequenceIterative
 {
+
+
+    public static int[][] Weave(int[] x, int[] y)
+    {
+        var accum = new List<int[]>();
+        for (var n = x.Length; n != 0; n--)
+        {
+            var c = x[n - 1];
+            var r = c.GetImmediateRightMember(x);
+            r.Match(None: () =>
+            {
+                for (var i = y.Length(); i != -1; i--)
+                {
+                    var injectedIntoOtherArray = y.Inject(c, i);
+                    accum.Add(injectedIntoOtherArray);
+                }
+            }, Some: sr =>
+            {
+
+            });
+        }
+
+        return accum.ToArray();
+    }
+        
 }
