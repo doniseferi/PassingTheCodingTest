08f1669ea78293cca90b134ecc9d8c5e9d74540e
Edonis Seferi <doni.seferi@gmail.com>
2024-06-18 21:44:40 +0100

    minor refaq bubble sort

--- PassingTheCodingTestQuestions/Sort/BubbleSort.cs
+++ PassingTheCodingTestQuestions/Sort/BubbleSort.cs
@@ -4,20 +4,15 @@ public class BubbleSort
 {
     public static int[] Sort(int[] arr)
     {
-        if (arr.Length == 1)
-            return arr;
-
         for (var swap = 0; swap < arr.Length; swap++)
         {
             for (var i = 0; i < arr.Length - 1; i++)
             {
-                var temp = arr[i];
-                if (arr[i] > arr[i + 1])
-                {
-                    temp = arr[i + 1];
-                    arr[i + 1] = arr[i];
-                    arr[i] = temp;
-                }
+                if (arr[i] <= arr[i + 1]) continue;
+                
+                var temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
             }
         }
 
